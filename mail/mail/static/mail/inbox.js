ec5162bea058c41fd79370e0dc4b71b6fba50d65
Reyan Asghar <iamreyanasghar@gmail.com>
2026-08-07 19:35:44 +0500

    project mail started

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,10 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#submit').addEventListener('click', send_email);
+
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -23,11 +25,29 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email() {
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: 'foo@example.com',
+      subject: 'Meeting time',
+      body: 'How about we meet tomorrow at 3pm?'
+    })
+  })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+      console.log(result);
+    });
+
 }
